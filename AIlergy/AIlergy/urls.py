4f4fd867420c4785537697696de6b99152036dc8
server user <server@instance-1.us-central1-a.c.beaming-age-252201.internal>
2019-09-08 02:59:57 +0000

    django url config

--- AIlergy/AIlergy/urls.py
+++ AIlergy/AIlergy/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.conf.urls import url
+from django.urls import path
 from app.views import *
 
 urlpatterns = [
-    path('request/', requestResponse),
+    path('request', requestResponse),
     path('admin/', admin.site.urls),
 ]
