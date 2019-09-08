c861f7296e57768977fd1616ac7a24f170c6f15e
Eric Dong <aderic6@gmail.com>
2019-09-07 21:08:36 -0400

    remove pages

--- AIlergy/AIlergy/urls.py
+++ AIlergy/AIlergy/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from app.views import *
 
 urlpatterns = [
-    path('', include('pages.urls')),
+    path('request/', requestResponse),
     path('admin/', admin.site.urls),
 ]
--- AIlergy/app/views.py
+++ AIlergy/app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import JsonResponse
+
+def requestResponse(request):
+    sentData = request.POST['data']
+    
+
